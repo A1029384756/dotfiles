43f0dc69e2ce925e448379658e987e950dd480a7
A1029384756 <hayden.gray104@gmail.com>
2023-06-30 14:57:34 -0400

    powershell windows setup

--- install_dots.ps1
+++ install_dots.ps1
@@ -4,5 +4,5 @@ $configDir = "$env:USERPROFILE\.config"
 
 foreach ($dot in $dotsToInstall) {
   $linkName = Join-Path $configDir $dot
-  New-Item -ItemType SymbolicLink -Path $linkName -Target (Join-Path $cwd "condif" $dot)
+  New-Item -ItemType SymbolicLink -Path $linkName -Target (Join-Path $cwd "config" $dot)
 }
