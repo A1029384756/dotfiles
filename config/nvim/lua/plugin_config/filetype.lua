a8784b9b1de98b8b5fb5e0d0f6da0e93c39b3dd3
Hayden Gray <hayden.gray104@gmail.com>
2024-02-05 17:49:59 -0500

    added snakemake support

--- config/nvim/lua/plugin_config/filetype.lua
+++ config/nvim/lua/plugin_config/filetype.lua
@@ -15,3 +15,9 @@ vim.api.nvim_create_autocmd(
     pattern = { '*.tpp', '*.txx' },
     command = 'setfiletype cpp',
   })
+
+vim.api.nvim_create_autocmd(
+  { 'BufEnter', 'BufWinEnter' }, {
+    pattern = { 'Snakefile'},
+    command = 'setfiletype python',
+  })
