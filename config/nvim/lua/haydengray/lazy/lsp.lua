bcd9d3f3c3c975223022ce793caeb75132c94670
A1029384756 <hayden.gray104@gmail.com>
2024-05-09 17:15:18 -0400

    removed manual odin setup

--- config/nvim/lua/haydengray/lazy/lsp.lua
+++ config/nvim/lua/haydengray/lazy/lsp.lua
@@ -8,7 +8,7 @@ return {
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-nvim-lua',
-'hrsh7th/cmp-nvim-lsp-signature-help',
+    'hrsh7th/cmp-nvim-lsp-signature-help',
     'L3MON4D3/LuaSnip',
     'onsails/lspkind.nvim',
     'saadparwaiz1/cmp_luasnip',
@@ -75,8 +75,6 @@ return {
         }
       end,
     })
-
-    lspconfig.ols.setup({})
 
     local cmp = require('cmp')
     local luasnip = require('luasnip')
