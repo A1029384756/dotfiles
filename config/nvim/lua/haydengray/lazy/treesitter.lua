8c534218e1644d8b86e0a26a1c8b0a5579652dc5
A1029384756 <hayden.gray104@gmail.com>
2024-07-17 14:11:28 -0400

    updated treesitter to highlight by default

--- config/nvim/lua/haydengray/lazy/treesitter.lua
+++ config/nvim/lua/haydengray/lazy/treesitter.lua
@@ -1,13 +1,15 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   priority = 500,
-  opts = {
-    ensure_installed = { 'rust', 'c', 'cpp', 'lua', 'odin' },
-    sync_install = false,
-    auto_install = true,
-    ignore_install = {},
-    highlight = { enable = true, },
-    indent = { enable = true, },
-    modules = {}
-  }
+  config = function()
+    require('nvim-treesitter.configs').setup {
+      ensure_installed = { 'rust', 'c', 'cpp', 'lua', 'odin' },
+      sync_install = false,
+      auto_install = true,
+      ignore_install = {},
+      highlight = { enable = true, },
+      indent = { enable = true, },
+      modules = {}
+    }
+  end
 }
