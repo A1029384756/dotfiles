388ae4782cfd935ae391bc0a940a6b6a5172f065
A1029384756 <hayden.gray104@gmail.com>
2024-07-19 18:25:52 -0400

    added noegit binding

--- config/nvim/lua/haydengray/lazy/init.lua
+++ config/nvim/lua/haydengray/lazy/init.lua
@@ -6,9 +6,9 @@ return {
     opts = {
       assets = {
         odin = {
-          name = "Odin", -- "Grug braining"
-          icon = "https://github.com/odin-lang/artwork/blob/master/logo/emblem-260.png?raw=true",
-          tooltop = "odin-lang.org",
+          name = 'Odin', -- 'Grug braining'
+          icon = 'https://github.com/odin-lang/artwork/blob/master/logo/emblem-260.png?raw=true',
+          tooltop = 'odin-lang.org',
           type = 0,
         },
       },
@@ -30,24 +30,28 @@ return {
     end,
   },
   {
-    "NeogitOrg/neogit",
+    'NeogitOrg/neogit',
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "sindrets/diffview.nvim",
-      "nvim-telescope/telescope.nvim",
+      'nvim-lua/plenary.nvim',
+      'sindrets/diffview.nvim',
+      'nvim-telescope/telescope.nvim',
     },
-    config = true,
+    config = function()
+      local ng = require('neogit')
+      ng.setup {}
+      vim.keymap.set('n', '<leader>ng', ng.open, { desc = 'open neogit' })
+    end,
   },
   {
     'lewis6991/gitsigns.nvim',
     opts = {
       signs = {
-        add = { text = "│" },
-        change = { text = "│" },
-        delete = { text = "󰍵" },
-        topdelete = { text = "‾" },
-        changedelete = { text = "~" },
-        untracked = { text = "│" },
+        add = { text = '│' },
+        change = { text = '│' },
+        delete = { text = '󰍵' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+        untracked = { text = '│' },
       },
       preview_config = {
         border = 'rounded',
@@ -63,9 +67,9 @@ return {
           return { buffer = bufnr, desc = desc }
         end
 
-        vim.keymap.set("n", "<leader>rh", gs.reset_hunk, opts "reset hunk")
-        vim.keymap.set("n", "<leader>ph", gs.preview_hunk, opts "preview hunk")
-        vim.keymap.set("n", "<leader>gb", gs.blame_line, opts "blame line")
+        vim.keymap.set('n', '<leader>rh', gs.reset_hunk, opts 'reset hunk')
+        vim.keymap.set('n', '<leader>ph', gs.preview_hunk, opts 'preview hunk')
+        vim.keymap.set('n', '<leader>gb', gs.blame_line, opts 'blame line')
       end,
     }
   },
